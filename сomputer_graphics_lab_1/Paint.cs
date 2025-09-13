567f450d8a30f55c78dc301688c7aa271c532acc
Sergey <rihlerstudy@gmail.com>
2025-09-13 19:37:21 +0300

    Дописание конструктора класса Paint

--- сomputer_graphics_lab_1/Paint.cs
+++ сomputer_graphics_lab_1/Paint.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace сomputer_graphics_lab_1
 {
@@ -10,18 +11,103 @@ namespace сomputer_graphics_lab_1
     {
         private Matrix<double> coords;
 
+        private List<List<int>> connections;
+
+        private Graphics g;
+        Pen pen = new Pen(Color.Black, 2);
+
         private Paint()
         {
             coords = new Matrix<double>(40, 3);
             coords[0, 0] = -1;
             coords[0, 1] = 20;
-            coords[0,2] = 1;
+            coords[0, 2] = 1;
             coords[1, 0] = -3;
             coords[1, 1] = 20;
             coords[1, 2] = 1;
-            coords[2, 0] = ;
-            coords[2, 1] = 20;
+            coords[2, 0] = -3;
+            coords[2, 1] = 12;
             coords[2, 2] = 1;
+            coords[3, 0] = -1;
+            coords[3, 1] = 12;
+            coords[3, 2] = 1;
+            coords[4, 0] = -1;
+            coords[4, 1] = 11;
+            coords[4, 2] = 1;
+            coords[5, 0] = -3;
+            coords[5, 1] = 11;
+            coords[5, 2] = 1;
+            coords[6, 0] = -3;
+            coords[6, 1] = 7;
+            coords[6, 2] = 1;
+            coords[7, 0] = -1;
+            coords[7, 1] = 7;
+            coords[7, 2] = 1;
+            coords[8, 0] = -1;
+            coords[8, 1] = 5;
+            coords[8, 2] = 1;
+            coords[9, 0] = -7;
+            coords[9, 1] = 5;
+            coords[9, 2] = 1;
+            coords[10, 0] = -7;
+            coords[10, 1] = -1;
+            coords[10, 2] = 1;
+            coords[11, 0] = -5;
+            coords[11, 1] = -1;
+            coords[11, 2] = 1;
+            coords[12, 0] = -5;
+            coords[12, 1] = 3;
+            coords[12, 2] = 1;
+            coords[13, 0] = -3;
+            coords[13, 1] = 3;
+            coords[13, 2] = 1;
+            coords[14, 0] = -3;
+            coords[14, 1] = -12;
+            coords[14, 2] = 1;
+            coords[15, 0] = -1;
+            coords[15, 1] = -12;
+            coords[15, 2] = 1;
+            coords[16, 0] = -1;
+            coords[16, 1] = -7;
+            coords[16, 2] = 1;
+            coords[17, 0] = -1;
+            coords[17, 1] = 9;
+            coords[17, 2] = 1;
+            coords[18, 0] = -1;
+            coords[18, 1] = 10;
+            coords[18, 2] = 1;
+            coords[19, 0] = -1;
+            coords[19, 1] = 14;
+            coords[19, 2] = 1;
+            for (int i = 20; i < 40; i++)
+            {
+                coords[i, 0] = coords[i - 20, 0] * -1;
+                coords[i, 1] = coords[i - 20, 1] * -1;
+                coords[i, 2] = coords[i - 20, 1];
+            }
+
+            connections = new List<List<int>>();
+            for (int i = 0; i < 40; i++)
+            {
+                if (i == 17 || i == 18 || i == 19 || i == 37 || i == 38 || i == 39)
+                connections.Add(new List<int>());
+                connections[connections.Count - 1] = new List<int>() { i, i+1 };
+            }
+            connections.Add(new List<int>());
+            connections[connections.Count - 1] = new List<int>() { 17, 18 };
+            connections.Add(new List<int>());
+            connections[connections.Count - 1] = new List<int>() { 18, 38 };
+            connections.Add(new List<int>());
+            connections[connections.Count - 1] = new List<int>() { 38, 37 };
+            connections.Add(new List<int>());
+            connections[connections.Count - 1] = new List<int>() { 37, 17 };
+            connections.Add(new List<int>());
+            connections[connections.Count - 1] = new List<int>() { 19, 39 };
+        }
+
+        private void draw()
+        {
+            Matrix<double> displayCoord = null;
 
         }
 
