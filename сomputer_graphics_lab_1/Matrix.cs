fbc4d42d0a45d3c471dc36e7a9056541bc7e1f3e
Sergey <rihlerstudy@gmail.com>
2025-09-14 11:55:48 +0300

    Исправление создания матрицы

--- сomputer_graphics_lab_1/Matrix.cs
+++ сomputer_graphics_lab_1/Matrix.cs
@@ -27,7 +27,12 @@ namespace сomputer_graphics_lab_1
             innerMatrix = new List<List<T>>();
             for (int i = 0; i < row; i++)
             {
-                innerMatrix.Add(new List<T>(col));
+                List<T> list = new List<T>(col); 
+                for (int j = 0; j < col; j++)
+                {
+                    list.Add(default(T)); 
+                }
+                innerMatrix.Add(list);
             }
         }
 
@@ -52,56 +57,56 @@ namespace сomputer_graphics_lab_1
 
             return result;
         }
+    }
 
-        public class rotationMatrix : Matrix<double>
+    public class rotationMatrix : Matrix<double>
+    {
+        public rotationMatrix(double angle) : base(3, 3)
         {
-            public rotationMatrix(double angle) : base(3, 3)
-            {
-                double sinAngle = Math.Sin(angle);
-                double cosAngle = Math.Cos(angle);
-                innerMatrix[0][0] = cosAngle;
-                innerMatrix[0][1] = sinAngle;
-                innerMatrix[0][2] = 0;
-                innerMatrix[1][0] = -sinAngle;
-                innerMatrix[1][1] = cosAngle;
-                innerMatrix[1][2] = 0;
-                innerMatrix[2][0] = 0;
-                innerMatrix[2][1] = 0;
-                innerMatrix[2][2] = 1;
+            double sinAngle = Math.Sin(angle);
+            double cosAngle = Math.Cos(angle);
+            innerMatrix[0][0] = cosAngle;
+            innerMatrix[0][1] = sinAngle;
+            innerMatrix[0][2] = 0;
+            innerMatrix[1][0] = -sinAngle;
+            innerMatrix[1][1] = cosAngle;
+            innerMatrix[1][2] = 0;
+            innerMatrix[2][0] = 0;
+            innerMatrix[2][1] = 0;
+            innerMatrix[2][2] = 1;
 
-            }
         }
+    }
 
-        public class dilatationMatrix: Matrix<double>
+    public class dilatationMatrix : Matrix<double>
+    {
+        public dilatationMatrix(double a, double b) : base(3, 3)
         {
-            public dilatationMatrix(double a, double b): base(3, 3)
-            {
-                innerMatrix[0][0] = a;
-                innerMatrix[0][1] = 0;
-                innerMatrix[0][2] = 0;
-                innerMatrix[1][0] = 0;
-                innerMatrix[1][1] = b;
-                innerMatrix[1][2] = 0;
-                innerMatrix[2][0] = 0;
-                innerMatrix[2][1] = 0;
-                innerMatrix[2][2] = 1;
-            }
+            innerMatrix[0][0] = a;
+            innerMatrix[0][1] = 0;
+            innerMatrix[0][2] = 0;
+            innerMatrix[1][0] = 0;
+            innerMatrix[1][1] = b;
+            innerMatrix[1][2] = 0;
+            innerMatrix[2][0] = 0;
+            innerMatrix[2][1] = 0;
+            innerMatrix[2][2] = 1;
         }
+    }
 
-        public class translationMatrix: Matrix<double>
+    public class translationMatrix : Matrix<double>
+    {
+        public translationMatrix(double a, double b) : base(3, 3)
         {
-            public translationMatrix(double a, double b): base(3, 3)
-            {
-                innerMatrix[0][0] = 1;
-                innerMatrix[0][1] = 0;
-                innerMatrix[0][2] = 0;
-                innerMatrix[1][0] = 0;
-                innerMatrix[1][1] = 1;
-                innerMatrix[1][2] = 0;
-                innerMatrix[2][0] = a;
-                innerMatrix[2][1] = b;
-                innerMatrix[2][2] = 1;
-            }
+            innerMatrix[0][0] = 1;
+            innerMatrix[0][1] = 0;
+            innerMatrix[0][2] = 0;
+            innerMatrix[1][0] = 0;
+            innerMatrix[1][1] = 1;
+            innerMatrix[1][2] = 0;
+            innerMatrix[2][0] = a;
+            innerMatrix[2][1] = b;
+            innerMatrix[2][2] = 1;
         }
     }
 }
