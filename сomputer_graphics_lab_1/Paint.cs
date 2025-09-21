8d31d93212da8ab827ea5b0db84ff1e7ef42b25d
Sergey <rihlerstudy@gmail.com>
2025-09-21 19:01:40 +0300

    Разделение основной матрицы на подматрицы: передняя и задняя части, лицо

--- сomputer_graphics_lab_1/Paint.cs
+++ сomputer_graphics_lab_1/Paint.cs
@@ -9,198 +9,174 @@ namespace сomputer_graphics_lab_1
 {
     class Paint
     {
-        private Matrix<double> coords;
+        private Matrix<double> front;
+        private Matrix<double> back;
+        private Matrix<double> face;
+        private Matrix<double> center;
 
-        public List<List<int>> connections;
-
-        private Graphics g;
-        Pen pen = new Pen(Color.Black, 2);
-
+        public List<List<int>> connectionsFront;
+        public List<List<int>> connectionsBack;
+        public List<List<int>> connectionsFace;
 
         public Paint()
         {
-            coords = new Matrix<double>(70, 4);
-
-            for(int i = 0; i < 41; i++) 
+            //Создание передней стороны
+            front = new Matrix<double>(28, 4);
+            for(int i = 0; i < 28; i++) 
             {
-                coords[i, 3] = 4;
+                front[i, 2] = 4;
+                front[i, 3] = 1;
+            }
+            front[0, 0] = -1;
+            front[0, 1] = 20;
+            front[1, 0] = -3;
+            front[1, 1] = 20;
+            front[2, 0] = -3;
+            front[2, 1] = 7;
+            front[3, 0] = -1;
+            front[3, 1] = 7;
+            front[4, 0] = -1;
+            front[4, 1] = 5;
+            front[5, 0] = -7;
+            front[5, 1] = 5;
+            front[6, 0] = -7;
+            front[6, 1] = -1;
+            front[7, 0] = -5;
+            front[7, 1] = -1;
+            front[8, 0] = -5;
+            front[8, 1] = 3;
+            front[9, 0] = -3;
+            front[9, 1] = 3;
+            front[10, 0] = -3;
+            front[10, 1] = -12;
+            front[11, 0] = -1;
+            front[11, 1] = -12;
+            front[12, 0] = -1;
+            front[12, 1] = -7;
+            front[13, 0] = -1;
+            front[13, 1] = 9;
+            for (int i = 14; i < 28; i++)
+            {
+                front[i, 0] = front[i - 14, 0] * -1;
+                front[i, 1] = front[i - 14, 1];
             }
 
-            // Точки задней стороны
-            for (int i = 0, j = 41; i < 41; i++, j++)
+            //Создание задней стороны
+            back = new Matrix<double>(28, 4);
+            for (int i = 0; i < 28; i++)
             {
-                if (i == 17 || i == 18 || i == 37 || i == 38 
-                   || i == 16 || i == 36 || i == 2 || i == 5 || i == 22 || i == 25 || i == 0)
-                {
-                    continue;
-                }
+                back[i, 0] = front[i, 2];
+                back[i, 1] = front[i, 3];
+                back[i, 3] = front[i, 2] * -1;
+                back[i, 2] = front[i, 3];
+            }
 
-                coords[j, 0] = coords[i, 0];
-                coords[j, 1] = coords[i, 1];
-                coords[j, 2] = -coords[i, 2];
-                coords[j, 3] = 1;
+            //Создание центра
+            center = new Matrix<double>(1, 4);
+            center[0, 0] = 0;
+            center[0, 1] = 0;
+            center[0, 2] = 0;
+            center[0, 3] = 1;
+
+            //Создание лица
+            face = new Matrix<double>(12, 4);
+            for(int i = 0; i < 12; i++) 
+            {
+                face[i, 3] = 4;
+                face[i, 4] = 1;
+            }
+            face[0, 0] = -3;
+            face[0, 1] = 12;
+            face[1, 0] = -1;
+            face[1, 1] = 12;
+            face[2, 0] = -1;
+            face[2, 1] = 11;
+            face[3, 0] = -3;
+            face[3, 1] = 11;
+            for(int i = 4; i < 8; i++)
+            {
+                face[i, 0] = -face[i - 4, 0];
+                face[i, 1] = face[i - 4, 1];
+            }
+            face[8, 0] = -1;
+            face[8, 1] = 10;
+            face[9, 0] = 1;
+            face[9, 1] = 10;
+            face[10, 0] = -1;
+            face[10, 1] = 9;
+            face[11, 0] = 1;
+            face[11, 1] = 9;
+
+            //Соединения передней стороны
+            connectionsFront = new List<List<int>>();
+            for (int i = 0; i < 27; i++)
+            {
+                connectionsFront.Add(new List<int>(){i, i+1});
+            }
+
+            //Соединения задней стороны
+            connectionsBack = new List<List<int>>();
+            for (int i = 0; i < 27; i++)
+            {
+                connectionsBack.Add(new List<int>() { i, i + 1 });
+            }
+
+            //Соединения лица
+            connectionsFace = new List<List<int>>();
+            for (int i = 0, j = 0 ; i < 11; i++, j++)
+            {
+                if (j == 3) { j = 0;  continue; }
+                connectionsFace.Add(new List<int>() { i, i + 1 });
             }
 
 
-            coords[0, 0] = -1;
-            coords[0, 1] = 20;
-            coords[0, 2] = 1;
-            coords[1, 0] = -3;
-            coords[1, 1] = 20;
-            coords[1, 2] = 1;
-            coords[2, 0] = -3;
-            coords[2, 1] = 12;
-            coords[2, 2] = 1;
-            coords[3, 0] = -1;
-            coords[3, 1] = 12;
-            coords[3, 2] = 1;
-            coords[4, 0] = -1;
-            coords[4, 1] = 11;
-            coords[4, 2] = 1;
-            coords[5, 0] = -3;
-            coords[5, 1] = 11;
-            coords[5, 2] = 1;
-            coords[6, 0] = -3;
-            coords[6, 1] = 7;
-            coords[6, 2] = 1;
-            coords[7, 0] = -1;
-            coords[7, 1] = 7;
-            coords[7, 2] = 1;
-            coords[8, 0] = -1;
-            coords[8, 1] = 5;
-            coords[8, 2] = 1;
-            coords[9, 0] = -7;
-            coords[9, 1] = 5;
-            coords[9, 2] = 1;
-            coords[10, 0] = -7;
-            coords[10, 1] = -1;
-            coords[10, 2] = 1;
-            coords[11, 0] = -5;
-            coords[11, 1] = -1;
-            coords[11, 2] = 1;
-            coords[12, 0] = -5;
-            coords[12, 1] = 3;
-            coords[12, 2] = 1;
-            coords[13, 0] = -3;
-            coords[13, 1] = 3;
-            coords[13, 2] = 1;
-            coords[14, 0] = -3;
-            coords[14, 1] = -12;
-            coords[14, 2] = 1;
-            coords[15, 0] = -1;
-            coords[15, 1] = -12;
-            coords[15, 2] = 1;
-            coords[16, 0] = -1;
-            coords[16, 1] = -7;
-            coords[16, 2] = 1;
-            coords[17, 0] = -1;
-            coords[17, 1] = 9;
-            coords[17, 2] = 1;
-            coords[18, 0] = -1;
-            coords[18, 1] = 10;
-            coords[18, 2] = 1;
-            coords[19, 0] = -1;
-            coords[19, 1] = 14;
-            coords[19, 2] = 1;
-            for (int i = 20; i < 40; i++)
-            {
-                coords[i, 0] = coords[i - 20, 0] * -1;
-                coords[i, 1] = coords[i - 20, 1];
-                coords[i, 2] = 1;
-            }
-            coords[40, 0] = 0;
-            coords[40, 1] = 0;
-            coords[40, 2] = 0;
-            coords[40, 3] = 1;
-
-
-            connections = new List<List<int>>();
-            for (int i = 0; i < 40; i++)
-            {
-                if (i == 17 || i == 18 || i == 19 || i == 37 || i == 38 || i == 39 
-                    || i == 16 || i == 36)
-                {
-                    continue;
-                }
-                connections.Add(new List<int> { i, i + 1 });
-            }
-            connections.Add(new List<int>() { 17, 18 });
-            connections.Add(new List<int>() { 18, 38 });
-            connections.Add(new List<int>() { 38, 37 });
-            connections.Add(new List<int>() { 37, 17 });
-            connections.Add(new List<int>() { 19, 39 });
-            connections.Add(new List<int>() { 16, 36 });
-            connections.Add(new List<int>() { 2, 5 });
-            connections.Add(new List<int>() { 22, 25 });
-            connections.Add(new List<int>() { 20, 39 });
-            connections.Add(new List<int>() { 0, 19 });
-
-            for (int i = 41; i < 69; i++) 
-            {
-                connections.Add(new List<int> { i, i + 1 });
-            }
-
-            for (int i = 0, step = 41; i < 18; i++) 
-            {
-                if (i == 2) { step = 37; i = 5; continue; }
-                connections.Add(new List<int>() { i, i + step });
-            }
-
-            for (int i = 20, step = 41; i < 36; i++) 
-            {
-                if (i == 22) { step = 37; i = 25; continue; }
-                connections.Add(new List<int>() { i, i + step });
-            }
-
-            connections.Add(new List<int>() { 19, 54 });
-            connections.Add(new List<int>() { 39, 69 });
         }
 
         public Matrix<double> transformMatrix(Panel paintPanel)
         {
-            Matrix<double> result = new Matrix<double>(coords.getRows(), coords.getCols());
+            Matrix<double> result = new Matrix<double>(front.getRows(), front.getCols());
             const int zoomPix = 10;
-            for (int i = 0; i < coords.getRows(); i++)
+            for (int i = 0; i < front.getRows(); i++)
             {
-                result[i, 0] = paintPanel.Width / 2 + zoomPix * coords[i, 0];
-                result[i, 1] = paintPanel.Height / 2 - zoomPix* coords[i, 1];
+                result[i, 0] = paintPanel.Width / 2 + zoomPix * front[i, 0];
+                result[i, 1] = paintPanel.Height / 2 - zoomPix* front[i, 1];
                 result[i, 2] = 1;
             }
             return result;
         }
         public List<double> getCenter()
         {
-            return new List<double>() { coords[40, 0], coords[40, 1], coords[40, 2] };
+            return new List<double>() { front[40, 0], front[40, 1], front[40, 2] };
         }
 
         public void move(double a, double b, double c)
         {
             translationMatrix matrix = new translationMatrix(a, b, c);
-            coords = coords.multiplyMatrix(matrix);
+            front = front.multiplyMatrix(matrix);
         }
 
         public void rotationXY(double a) 
         {
             rotationMatrixXY matrix = new rotationMatrixXY(a);
-            coords = coords.multiplyMatrix(matrix);
+            front = front.multiplyMatrix(matrix);
         }
 
         public void rotationXZ(double a)
         {
             rotationMatrixXZ matrix = new rotationMatrixXZ(a);
-            coords = coords.multiplyMatrix(matrix);
+            front = front.multiplyMatrix(matrix);
         }
 
         public void rotationYZ(double a)
         {
             rotationMatrixYZ matrix = new rotationMatrixYZ(a);
-            coords = coords.multiplyMatrix(matrix);
+            front = front.multiplyMatrix(matrix);
         }
 
         public void zoom(double a, double b, double c)
         {
             dilatationMatrix matrix = new dilatationMatrix(a, b, c);
-            coords = coords.multiplyMatrix(matrix);
+            front = front.multiplyMatrix(matrix);
         }
     }
 }
