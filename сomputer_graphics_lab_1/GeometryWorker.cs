b69a203625479e5877ccf7ee62839cd74450e415
Sergey <rihlerstudy@gmail.com>
2025-10-12 12:49:49 +0300

    Добавлена функция рассчета коэффициентов линейного уравнения прямой

---
+++ сomputer_graphics_lab_1/GeometryWorker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace сomputer_graphics_lab
+{
+    static class GeometryWorker
+    {
+        public static (double, double) findCoeffLinearEquation(Matrix<double> dots, int indFirst, int indSecond, Plane plane)
+        {
+            int firstAxis = 0;
+            int secondAxis = 1;
+            switch (plane)
+            {
+                case Plane.X:
+                    firstAxis = 1;
+                    secondAxis = 2;
+                    break;
+                case Plane.Y:
+                    firstAxis = 0;
+                    secondAxis = 2;
+                    break;
+                case Plane.Z:
+                    firstAxis = 0;
+                    secondAxis = 1;
+                    break;
+                default: 
+                    break;
+            }
+            double l = dots[indFirst, firstAxis] - dots[indSecond, firstAxis] / (dots[indFirst, secondAxis] - dots[indSecond, secondAxis]);
+            double m = dots[indFirst, secondAxis] - l * dots[indFirst, firstAxis];
+            return (l, m);
+        }
+
+
+
+    }
+}
